ffe306386a5319d3cfe553558443eabbe9c2427e
Hao Fu <haofu@ucdavis.edu>
2017-09-15 22:04:42 -0400

    durations

--- data_collection/src/tmp.py
+++ data_collection/src/tmp.py
@@ -1,13 +1,34 @@
-#!/usr/bin/env python2
-# -*-encoding:utf-8-*-
-
-from uiautomator import Device
+from PacpHandler import PcapHandler
 import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as dt
+import json
 
+def event_duration(out_dir):
+        timestamps = []
+        mins = []
+        maxs = []
+        index = []
+        for root, dirs, files in os.walk(out_dir, topdown=True):
+            for name in files:
+                # print(os.path.join(root, name))
+                if str(name).endswith('.pcap'):
+                    pcap_path = os.path.join(root, name)
+                    min, max = PcapHandler.duration_pcap(pcap_path)
+                    mins.append(min)
+                    maxs.append(max)
+                    index.append(1)
+        data = dict()
+        data['index'] = index
+        data['mins'] = mins
+        data['maxs'] = maxs
+        json.dumps(data, os.path.join(out_dir, 'timestamps.json'))
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        #ax = ax.xaxis_date()
+        ax = plt.hlines(index, mins, maxs)
+        fig.show()
 
-
-if __name__ == '__main__':
-    statinfo = os.stat('C:\Users\majes\PycharmProjects\TrafficAnalysis'
-                       '\data_collection\output\AnserverBot'
-                       '\\30441d6f70304fdefb84d983a99a99a1007d25de\\com.keji.danti6000708-11-39-28.pcap')
-    print statinfo.st_size
+#event_duration('/mnt/Documents/flows/Event/147-32-84-165/TCP-CC')
+event_duration('/mnt/Documents/FlowIntent/output/test/2421536307fd9a885cc66c58419cea2e307620dfb67ab96f11aa33380da14c93/')
--- ml/src/tmp.py
+++
@@ -1,3 +0,0 @@
-from CtuCCAnalyzer import LatexTableGenerator
-
-LatexTableGenerator.event_duration('C:\Users\hfu\Documents\\flows\CTU-13\CTU-13-9\\0\TCP-CC')
